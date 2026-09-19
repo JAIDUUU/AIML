8b3bbe369ef6a8a09f2dfbe01b263e2c87911b16
JAIDUUU <jaidzxxd@gmail.com>
2026-09-20 01:29:36 +0530

    Learn NumPy array creation from lists
    2

---
+++ NumPy/02_Creating_Arrays/eye.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+#identity matrix
+arr=np.eye(3)
+print(arr)
---
+++ NumPy/02_Creating_Arrays/linspace.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#do numbers kei beech equal spacing
+
+arr=np.linspace(1,10,5)
+print(arr)
+
+#1 = start
+# 10 = end
+# 5 = kitni values chahiye
